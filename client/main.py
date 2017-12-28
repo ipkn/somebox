73876a3e68d21eabcd6d49500b7e086fb0b227c0
Jaeseung Ha <ipknhama@gmail.com>
2017-12-29 01:06:34 +0900

    move watch code to watcher

--- client/main.py
+++ client/main.py
@@ -1,39 +1,9 @@
-import time
-import os
 import settings
+from watcher import Watcher
 
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-class EventHandler(FileSystemEventHandler):
-    def on_created(self, event):
-        super().on_created(event)
-        print('+', event)
-    def on_deleted(self, event):
-        super().on_deleted(event)
-        print('-', event)
-    def on_modified(self, event):
-        super().on_modified(event)
-        print('*', event)
-    def on_moved(self, event):
-        super().on_moved(event)
-        print('M', event)
-
-def main(watch_folders, EventHandlerClass = EventHandler, is_done = lambda : False):
-    print(EventHandlerClass)
-    event_handler = EventHandlerClass()
-    observer = Observer()
-    for path in watch_folders:
-        print(path)
-        observer.schedule(event_handler, path, recursive = True)
-    observer.start()
-
-    try:
-        while not is_done():
-            time.sleep(0.1)
-        observer.stop()
-    except KeyboardInterrupt:
-        observer.stop()
+def main(watch_folders):
+    w = Watcher(watch_folders)
+    w.run()
 
 if __name__ == '__main__':
     main(settings.watch_folders)
---
+++ client/watcher.py
@@ -0,0 +1,47 @@
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class EventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        super().on_created(event)
+        print('+', event)
+    def on_deleted(self, event):
+        super().on_deleted(event)
+        print('-', event)
+    def on_modified(self, event):
+        super().on_modified(event)
+        print('*', event)
+    def on_moved(self, event):
+        super().on_moved(event)
+        print('M', event)
+
+class Watcher:
+    EventHandlerClass = EventHandler
+    def __init__(self, watch_folders):
+        self.event_handler = self.EventHandlerClass()
+        self.observer = Observer()
+        self.done = False
+
+        self.watch_folders = watch_folders
+
+        for path in watch_folders:
+            self.observer.schedule(self.event_handler, path, recursive = True)
+
+    def set_done(self):
+        self.done = True
+
+    def is_done(self):
+        return self.done
+
+    def run(self):
+        observer = self.observer
+        observer.start()
+
+        try:
+            while not self.is_done():
+                time.sleep(0.1)
+            observer.stop()
+        except KeyboardInterrupt:
+            observer.stop()
